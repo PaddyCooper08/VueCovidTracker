96c9d4ddf29eb6a21b3c8a5b041474bfd0227ae4
Paddy Cooper <1210@rgsg.co.uk>
2021-10-21 20:56:02 +0100

    Tad more setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "/src/components/Home.vue";
+import Home from "/src/views/Home.vue";
 const routes = [
   {
     path: "/",
@@ -11,4 +11,3 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-export default router;
