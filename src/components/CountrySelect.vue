23662f93e1b0a69ac5890d3428f491633d6d71ac
Paddy Cooper <1210@rgsg.co.uk>
2021-10-22 10:58:05 +0100

    Finishing touches

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <select
+    v-model="selected"
+    class="
+      text-blue-900
+      form-select
+      mt-10
+      block
+      w-full
+      border
+      p-3
+      rounded
+      border-blue-300
+    "
+    @change="onChange()"
+  >
+    <option value="0">Select Country</option>
+    <option :value="country.ID" v-for="country in countries">
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected);
+
+      this.$emit("get-country", country);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,17 +15,37 @@
   <main v-else>
     <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <div class="flex justify-center items-center">
+      <button
+        v-if="stats.Country"
+        class="
+          bg-green-700
+          text-white
+          rounded
+          p-3
+          mt-10
+          focus:outline-none
+          hover:bg-green-600
+        "
+        @click="onClick"
+      >
+        Clear Country
+      </button>
+    </div>
   </main>
 </template>
 <script>
 import DataTitle from "../components/DataTitle.vue";
 import DataBoxes from "../components/DataBoxes.vue";
+import CountrySelect from "../components/CountrySelect.vue";
 
 export default {
   name: "Home",
   components: {
     DataTitle,
     DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -41,6 +61,18 @@ export default {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
+    },
+
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async onClick() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
